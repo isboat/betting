4fc8f00e57b7b123aedabdc12a5d8477b624090f
Isaac Boateng <iboateng@sportingindex.com>
2017-06-27 11:55:41 +0100

    Initial push

---
+++ Betting/Betting.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Betting.Web.Startup))]
+namespace Betting.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
